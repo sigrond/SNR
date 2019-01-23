db0865817403803849ee5374a28755c293b70709
Tomasz Jakubczyk <sigrond93@gmail.com>
2019-01-23 16:12:11 +0100

    podpisy warstw

--- test0.m
+++ test0.m
@@ -7,40 +7,41 @@ tbl = countEachLabel(imds)
 [trainingSet,validationSet, testSet] = splitEachLabel(imds, 0.5, 0.25, 0.25, 'randomized');
 
 layers = [
-    imageInputLayer([227 227 3])
+    imageInputLayer([227 227 3],'Name','Image Input 227x227x3')
     %warstwa 1
-    convolution2dLayer(11,96,'Stride',4,'Padding',0)
-    reluLayer
-    maxPooling2dLayer(3,'Stride',2)
+    convolution2dLayer(11,96,'Stride',4,'Padding',0,'Name','Convolution 96 11x11 stride [4  4] padding [0  0  0  0]')
+    reluLayer('Name','1. ReLU')
+    maxPooling2dLayer(3,'Stride',2,'Name','1. Max Pooling 3x3 stride [2  2] padding [0  0  0  0]')
     %warstwa 2
-    crossChannelNormalizationLayer(5,'K',1)
-    convolution2dLayer(5,256,'Stride',1,'Padding',2)
-    reluLayer
-    maxPooling2dLayer(3,'Stride',2)
+    crossChannelNormalizationLayer(5,'K',1,'Name','2. Cross Channel Normalization with 5 channels per element')
+    convolution2dLayer(5,256,'Stride',1,'Padding',2,'Name','Convolution 256 5x5 stride [1  1] padding [2  2  2  2]')
+    reluLayer('Name','2. ReLU')
+    maxPooling2dLayer(3,'Stride',2,'Name','2. Max Pooling 3x3 stride [2  2] padding [0  0  0  0]')
     %warstwa 3
-    crossChannelNormalizationLayer(5,'K',1)
-    convolution2dLayer(3,384,'Stride',1,'Padding',1)
-    reluLayer
+    crossChannelNormalizationLayer(5,'K',1,'Name','3. Cross Channel Normalization with 5 channels per element')
+    convolution2dLayer(3,384,'Stride',1,'Padding',1,'Name','3. Convolution 384 3x3 stride [1  1] padding [1  1  1  1]')
+    reluLayer('Name','3. ReLU')
     %warstwa 4
-    convolution2dLayer(3,384,'Stride',1,'Padding',1)
-    reluLayer
+    convolution2dLayer(3,384,'Stride',1,'Padding',1,'Name','4. Convolution 384 3x3 stride [1  1] padding [1  1  1  1]')
+    reluLayer('Name','4. ReLU')
     %warstwa 5
-    convolution2dLayer(3,256,'Stride',1,'Padding',1)
-    reluLayer
-    maxPooling2dLayer(3,'Stride',2)
+    convolution2dLayer(3,256,'Stride',1,'Padding',1,'Name','Convolution 256 3x3 stride [1  1] padding [1  1  1  1]')
+    reluLayer('Name','5. ReLU')
+    maxPooling2dLayer(3,'Stride',2,'Name','5. Max Pooling 3x3 stride [2  2] padding [0  0  0  0]')
     %warstwa 6
-    fullyConnectedLayer(4096)
-    reluLayer
-    dropoutLayer
+    fullyConnectedLayer(4096,'Name','6. Fully Connected 4096 layer')
+    reluLayer('Name','6. ReLU')
+    dropoutLayer('Name','6. Dropout 50%')
     %warstwa 7
-    fullyConnectedLayer(4096)
-    reluLayer
-    dropoutLayer
+    fullyConnectedLayer(4096,'Name','7. Fully Connected 4096 layer')
+    reluLayer('Name','7. ReLU')
+    dropoutLayer('Name','7. Dropout 50%')
     %warstwa 8
-    fullyConnectedLayer(32)
-    softmaxLayer
-    classificationLayer
+    fullyConnectedLayer(32,'Name','Fully Connected 32 layer')
+    softmaxLayer('Name','Softmax')
+    classificationLayer('Name','Classification Output crossentropyex')
  ]
+lgraph = layerGraph(layers)
 
 imageSize = [227 227 3];
 augmentedTrainingSet = augmentedImageDatastore(imageSize, trainingSet, 'ColorPreprocessing', 'gray2rgb');
